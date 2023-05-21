5488b33923d843df05b37f899c1c077ad8a655a7
Akshay7016 <asajgule@gmail.com>
2023-05-21 13:03:19 +0530

    Course Controller updated

--- controllers/Course.js
+++ controllers/Course.js
@@ -10,21 +10,51 @@ const fileUploader = require("../utils/fileUploader");
 exports.createCourse = async (req, res) => {
     try {
         // fetch data from request body
-        const { courseName, courseDescription, whatYouWillLearn, price, category, tags } = req.body;
+        const {
+            courseName,
+            courseDescription,
+            whatYouWillLearn,
+            price,
+            category,
+            tags,
+            status,
+            instructions
+        } = req.body;
 
         // get thumbnail
         const thumbnail = req.files.thumbnailImage;
 
         // validation
-        if (!courseName || !courseDescription || !whatYouWillLearn || !price || !category || !tags || !thumbnail) {
+        if (
+            !courseName ||
+            !courseDescription ||
+            !whatYouWillLearn ||
+            !price ||
+            !category ||
+            !tags ||
+            !thumbnail
+        ) {
             return res.status(400).json({
                 success: false,
                 message: "All fields are required"
             })
         };
 
+        if (!status || status === undefined) {
+            status = "Draft";
+        }
+
         // take instructor id (user who logged-in is Instructor) from req.user
         const instructorId = req.user.id;
+
+        const instructorDetails = await User.findById(instructorId);
+
+        if (!instructorDetails) {
+            return res.status(404).json({
+                success: false,
+                message: "Instructor Details Not Found",
+            });
+        }
 
         // validation on category -> require for backend testing -> may be get invalid category id from request
         const categoryDetails = await Category.findById({ _id: category });
@@ -48,7 +78,9 @@ exports.createCourse = async (req, res) => {
             price,
             category,
             tags,
-            thumbnail: thumbnailImage.secure_url
+            thumbnail: thumbnailImage.secure_url,
+            status: status,
+            instructions: instructions,
         });
 
         // add the newCourse id to the User Schema -> courses field
@@ -83,7 +115,8 @@ exports.createCourse = async (req, res) => {
     } catch (error) {
         return res.status(500).json({
             success: false,
-            message: "Something went wrong while creating course"
+            message: "Something went wrong while creating course",
+            error: error.message
         });
     }
 };
@@ -96,7 +129,8 @@ exports.getAllCourses = async (req, res) => {
             price: true,
             thumbnail: true,
             instructor: true,
-            tags: true
+            ratingAndReviews: true,
+            studentsEnrolled: true
         }).populate("instructor").exec();
 
         res.status(200).json({
@@ -107,7 +141,9 @@ exports.getAllCourses = async (req, res) => {
     } catch (error) {
         return res.status(500).json({
             success: false,
-            message: "Something went wrong while fetching all courses"
+            message: "Something went wrong while fetching all courses",
+            error: error.message
+
         });
     }
 };
