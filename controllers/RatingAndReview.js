e6ed70698fe229199421ced1ee0170934a170920
Akshay7016 <asajgule@gmail.com>
2023-05-22 23:41:12 +0530

    getAverageRating controller added

--- controllers/RatingAndReview.js
+++ controllers/RatingAndReview.js
@@ -1,3 +1,5 @@
+const mongoose = require("mongoose");
+
 const RatingAndReview = require("../models/RatingAndReview");
 const Course = require("../models/Course");
 
@@ -65,6 +67,52 @@ exports.createRatingAndReview = async (req, res) => {
         return res.status(500).json({
             success: false,
             message: "Something went wrong while creating rating and review for course",
+            error: error.message
+        });
+    }
+};
+
+// getAverageRating
+exports.getAverageRating = async (req, res) => {
+    try {
+        // get course id
+        const { courseId } = req.body;
+
+        // calculate average rating using aggregate function. Aggregate function returns array
+        const result = await RatingAndReview.aggregate([
+            {
+                $match: {
+                    course: new mongoose.Types.ObjectId(courseId)
+                }
+            },
+            {
+                // if we don't know on which property we want group then specify null
+                $group: {
+                    _id: null,
+                    averageRating: { $avg: "$rating" }
+                }
+            }
+        ]);
+
+        // return rating
+        if (result.length > 0) {
+            return res.status(200).json({
+                success: true,
+                message: "Average rating fetched successfully",
+                data: rating[0].averageRating
+            });
+        };
+
+        // if no rating found for the course
+        return res.status(200).json({
+            success: true,
+            message: "Average rating is 0, No rating has been given till now for the course",
+            data: 0
+        });
+    } catch (error) {
+        return res.status(500).json({
+            success: false,
+            message: "Something went wrong while fetching average rating and review for course",
             error: error.message
         });
     }
