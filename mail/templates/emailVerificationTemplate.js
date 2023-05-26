a8d00d8290b3271417174f6431d701737f6884f3
Akshay7016 <asajgule@gmail.com>
2023-05-26 14:52:27 +0530

    email templates updated

--- mail/templates/emailVerificationTemplate.js
+++ mail/templates/emailVerificationTemplate.js
@@ -1,87 +1,103 @@
 const otpTemplate = (otp) => {
 	return `<!DOCTYPE html>
 	<html>
-	
-	<head>
-		<meta charset="UTF-8">
+	  <head>
+		<meta charset="UTF-8" />
 		<title>OTP Verification Email</title>
 		<style>
-			body {
-				background-color: #ffffff;
-				font-family: Arial, sans-serif;
-				font-size: 16px;
-				line-height: 1.4;
-				color: #333333;
-				margin: 0;
-				padding: 0;
-			}
+		  body {
+			background-color: #ffffff;
+			font-family: Arial, sans-serif;
+			font-size: 16px;
+			line-height: 1.4;
+			color: #333333;
+			margin: 0;
+			padding: 0;
+		  }
 	
-			.container {
-				max-width: 600px;
-				margin: 0 auto;
-				padding: 20px;
-				text-align: center;
-			}
+		  .container {
+			max-width: 600px;
+			margin: 0 auto;
+			padding: 20px;
+			text-align: center;
+		  }
 	
-			.logo {
-				max-width: 200px;
-				margin-bottom: 20px;
-			}
+		  .logo {
+			max-width: 200px;
+			margin-bottom: 20px;
+			padding: 10px;
+			border-radius: 10px;
+			background-color: #004687;
+		  }
 	
-			.message {
-				font-size: 18px;
-				font-weight: bold;
-				margin-bottom: 20px;
-			}
+		  .message {
+			font-size: 18px;
+			font-weight: bold;
+			margin-bottom: 20px;
+		  }
 	
-			.body {
-				font-size: 16px;
-				margin-bottom: 20px;
-			}
+		  .body {
+			font-size: 16px;
+			margin-bottom: 20px;
+		  }
 	
-			.cta {
-				display: inline-block;
-				padding: 10px 20px;
-				background-color: #FFD60A;
-				color: #000000;
-				text-decoration: none;
-				border-radius: 5px;
-				font-size: 16px;
-				font-weight: bold;
-				margin-top: 20px;
-			}
+		  .cta {
+			display: inline-block;
+			padding: 10px 20px;
+			background-color: #ffd60a;
+			color: #000000;
+			text-decoration: none;
+			border-radius: 5px;
+			font-size: 16px;
+			font-weight: bold;
+			margin-top: 20px;
+		  }
 	
-			.support {
-				font-size: 14px;
-				color: #999999;
-				margin-top: 20px;
-			}
+		  .support {
+			font-size: 14px;
+			color: #999999;
+			margin-top: 20px;
+		  }
 	
-			.highlight {
-				font-weight: bold;
-			}
+		  .highlight {
+			font-weight: bold;
+		  }
 		</style>
+	  </head>
 	
-	</head>
-	
-	<body>
+	  <body>
 		<div class="container">
-			<a href="https://studynotion-edtech-project.vercel.app"><img class="logo"
-					src="https://i.ibb.co/7Xyj3PC/logo.png" alt="StudyNotion Logo"></a>
-			<div class="message">OTP Verification Email</div>
-			<div class="body">
-				<p>Dear User,</p>
-				<p>Thank you for registering with StudyNotion. To complete your registration, please use the following OTP
-					(One-Time Password) to verify your account:</p>
-				<h2 class="highlight">${otp}</h2>
-				<p>This OTP is valid for 5 minutes. If you did not request this verification, please disregard this email.
-				Once your account is verified, you will have access to our platform and its features.</p>
-			</div>
-			<div class="support">If you have any questions or need assistance, please feel free to reach out to us at <a
-					href="mailto:info@studynotion.com">info@studynotion.com</a>. We are here to help!</div>
+		  <a href="https://studynotion-edtech-project.vercel.app"
+			><img
+			  class="logo"
+			  src="https://res.cloudinary.com/dmwje3sr4/image/upload/v1685078634/StudyWeb%20logo/final_fphd37.png"
+			  alt="StudyWeb Logo"
+		  /></a>
+		  <div class="message">OTP Verification Email</div>
+		  <div class="body">
+			<p>Dear User,</p>
+			<p>
+			  Thank you for registering with StudyWeb. To complete your
+			  registration, please use the following OTP (One-Time Password) to
+			  verify your account:
+			</p>
+			<h2 class="highlight">${otp}</h2>
+			<p>
+			  This OTP is valid for 5 minutes. If you did not request this
+			  verification, please disregard this email. Once your account is
+			  verified, you will have access to our platform and its features.
+			</p>
+		  </div>
+		  <div class="support">
+			If you have any questions or need assistance, please feel free to reach
+			out to us at
+			<a href="mailto:studyweb.contact@gmail.com"
+			  >studyweb.contact@gmail.com</a
+			>. We are here to help!
+		  </div>
 		</div>
-	</body>
-	
-	</html>`;
+	  </body>
+	</html>
+	`;
 };
 module.exports = otpTemplate;
